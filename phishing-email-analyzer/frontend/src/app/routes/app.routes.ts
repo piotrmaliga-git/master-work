e08cee7f088818ca82337fd2e3038e4a1fc0f9b8
piotrmaliga-git <piotr.maliga.work@gmail.com>
2026-02-27 20:40:28 +0100

    feat: add application routes

---
+++ phishing-email-analyzer/frontend/src/app/routes/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('pages/home/home.component').then((m) => m.HomePageComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('pages/not-found/not-found.component').then((m) => m.NotFoundPageComponent),
+  },
+];
