4f55ee6779bf0e976cafd62790ebcc6743f71286
piotrmaliga-git <piotr.maliga.work@gmail.com>
2026-03-15 15:42:25 +0100

    feat: add PrimeNG and animations configuration to app setup

--- phishing-email-analyzer/frontend/src/app/app.config.ts
+++ phishing-email-analyzer/frontend/src/app/app.config.ts
@@ -5,6 +5,9 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './routes/app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -16,5 +19,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
   ],
 };
