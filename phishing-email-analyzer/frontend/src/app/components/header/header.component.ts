fe6e10fda49dd69e1b45a29959fe544834626ad2
piotrmaliga-git <piotr.maliga.work@gmail.com>
2026-03-15 16:02:30 +0100

    feat: implement theme toggle functionality in header component and enhance styles for dark mode

--- phishing-email-analyzer/frontend/src/app/components/header/header.component.ts
+++ phishing-email-analyzer/frontend/src/app/components/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'page-header',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ButtonModule],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(readonly theme: ThemeService) {}
+
+  toggleTheme() {
+    this.theme.toggleDarkMode(!this.theme.isDark());
+  }
+}
